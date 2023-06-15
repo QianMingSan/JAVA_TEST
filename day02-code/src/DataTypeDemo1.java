ef5b419c92dc5eb2084fc12714327bdec41d2a4f
qianms <18153616873@163.com>
2023-06-15 16:44:48 +0800

    第二天练习

---
+++ day02-code/src/DataTypeDemo1.java
@@ -0,0 +1,28 @@
+public class DataTypeDemo1 {
+    /**
+     * 基本数据类型
+     *
+     * 整数  字节
+     * byte  1
+     * short 2
+     * int   4
+     * long  8
+     * 小数
+     * float  4
+     * double 8
+     * 字符
+     * char 2
+     * 布尔
+     * boolean 1
+     *---------------------
+     * 所有的整数都是int类型 所有的小数都是double类型
+     * ---------------------
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        //ascll 编码表
+        char c = 97;
+        System.out.println(c);
+    }
+}
