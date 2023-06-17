4dd52347ac131b9a9bb56f7427ce446783ca7873
qianmingsan(mac) <18153616873@163.com>
2023-06-17 21:24:18 +0800

    第三天

---
+++ day04-code/src/mfor/ForDemo1.java
@@ -0,0 +1,22 @@
+package mfor;
+
+public class ForDemo1 {
+    public static void main(String[] args) {
+    /**
+     * 格式
+     *
+     *
+     * 执行流程
+     *  1.初始化语句：定义变量 int i = 1;
+     *  2.执行判断条件 并看其返回的结果 如果 true继续执行 执行循环体语句 如果false 循环结束
+     *  3.执行条件控制语句 i++
+     *
+     *
+     */
+        for (int i = 1; i <=3; i++) {
+            System.out.println("跑圈");
+        }
+
+
+    }
+}
---
+++ day04-code/src/mswitch/SwitchDemo1.java
@@ -0,0 +1,23 @@
+package mswitch;
+
+public class SwitchDemo1 {
+    public static void main(String[] args) {
+        /**
+         *格式
+         */
+        int a  =2;
+        switch (a){
+
+            case 1:
+                System.out.println("春天");
+                break;
+            case 2:
+                System.out.println("秋天");
+                break;
+            default:
+                System.out.println("冬天");
+                break;
+
+        }
+    }
+}
