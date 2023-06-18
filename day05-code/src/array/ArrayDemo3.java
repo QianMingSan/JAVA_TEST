f939feb39c7c23adb53c556a7cd86cda807717f1
qianmingsan(mac) <18153616873@163.com>
2023-06-18 19:35:14 +0800

    第三天

--- day05-code/src/array/ArrayDemo3.java
+++ day05-code/src/array/ArrayDemo3.java
@@ -1,4 +1,16 @@
 package array;
 
 public class ArrayDemo3 {
+    public static void main(String[] args) {
+        /**
+         * 动态初始化
+         格式：数据类型[] 数组名 = new 数据类型[数组长度];
+         范例：int[] arr  = new int[3];
+         */
+
+        int[] arr  = new int[3];
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
 }
