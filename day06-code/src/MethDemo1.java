f35828becd788f3e70d8789f4ce4dd05064284c7
qianmingsan(mac) <18153616873@163.com>
2023-06-19 22:04:20 +0800

    第三天

---
+++ day06-code/src/MethDemo1.java
@@ -0,0 +1,14 @@
+public class MethDemo1 {
+    /**
+     * 方法的格式
+     * @param args
+     */
+    public static void main(String[] args) {
+        eat();
+    }
+
+    public static void eat(){
+        System.out.println("吃饭");
+    }
+}
+
