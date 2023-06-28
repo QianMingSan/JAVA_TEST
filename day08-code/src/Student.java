309c70d335aafd2cd2e472b64a2557dbbf15bc85
qianmingsan(mac) <18153616873@163.com>
2023-06-28 20:20:26 +0800

    第7天

---
+++ day08-code/src/Student.java
@@ -0,0 +1,29 @@
+public class Student {
+
+    /**
+     * 面向对象
+     *
+     *设计图纸 描述一件事物
+     *
+     * 学生叫张三 年龄20 身高180 循喜欢吃饭和学习
+     *
+     */
+    private String name ="张三";
+
+    private String age ="20";
+
+    private String heigh ="180";
+
+
+    public   void eat(){
+        System.out.println("吃饭");
+    }
+    public  void statudent(){
+        System.out.println("学习");
+    }
+
+
+
+
+
+}
